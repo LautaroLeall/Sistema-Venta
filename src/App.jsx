96ef80effa288b0843fe425352aa6117327be129
LautaroLeall <lautaroleal4@gmail.com>
2025-05-15 14:06:07 -0300

    Inicializando Proyecto

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import {GlobalStyles} from './index.js'
+
+function App() {
+
+  return (
+    <div>
+      <GlobalStyles />
+    </div>
+  )
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,3 @@
+export { default as App } from './App.jsx'
+export * from './main.jsx'
+export * from './styles/GlobalStyles.jsx'
